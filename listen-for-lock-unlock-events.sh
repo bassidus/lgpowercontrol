c9ef4704f7bb29f418997be917943d0c97fd0cd1
bassidus <basse+github@pm.me>
2025-06-21 13:18:52 +0200

    added logging and some refactoring

--- listen-for-lock-unlock-events.sh
+++ listen-for-lock-unlock-events.sh
@@ -1,27 +1,42 @@
 #!/bin/bash
 
+log() {
+    echo "$(date '+%F %T') $1" | logger --tag listen-for-lock-unlock-events
+}
+
+power_off_tv() {
+    local MAX_RETRIES=10
+    local DELAY=1
+    local COUNT=1
+    until <PWR_OFF_CMD>; do
+        ((COUNT++))
+        if [ "$COUNT" -gt "$MAX_RETRIES" ]; then
+            log "ERROR: Gave up after $MAX_RETRIES failed attempts to power OFF TV"
+            break
+        fi
+        sleep $DELAY
+    done
+}
+
+power_on_tv() {
+    local MAX_RETRIES=10
+    local DELAY=1
+    local COUNT=1
+    until <PWR_ON_CMD>; do
+        ((COUNT++))
+        if [ "$COUNT" -gt "$MAX_RETRIES" ]; then
+            log "ERROR: Gave up after $MAX_RETRIES failed attempts to send Wake-on-LAN packet"
+            break
+        fi
+        sleep $DELAY
+    done
+}
+
 # Listen for screen saver (lock/unlock) events over DBus
 dbus-monitor --session "type='signal',interface='org.freedesktop.ScreenSaver'" |
     while read -r x; do
         case "$x" in
-        *"boolean true"*)
-            MAX_RETRIES=10
-            DELAY=5
-            COUNT=1
-            until <PWR_OFF_CMD>; do
-                ((COUNT++))
-                if [ "$COUNT" -gt "$MAX_RETRIES" ]; then
-                    echo "ERROR: Gave up after $MAX_RETRIES failed attempts to power OFF TV"
-                    break
-                fi
-                sleep $DELAY
-            done
-            ;;
-
-        *"boolean false"*)
-            if ! <PWR_ON_CMD>; then
-                echo "ERROR: Failed to send Wake-on-LAN packet"
-            fi
-            ;;
+        *"boolean true"*)  power_off_tv ;; # Screen lock
+        *"boolean false"*) power_on_tv  ;; # Screen unlock
         esac
     done
